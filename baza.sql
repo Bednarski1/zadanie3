8b9878467a64098808f96dc5115f8b4dc88767f8
Bednarski1 <151836347+Bednarski1@users.noreply.github.com>
2023-11-23 14:29:39 +0100

    baza.sql

--- baza.sql
+++ baza.sql
@@ -1,11 +1,11 @@
-create database formularz;
+CREATE DATABASE formularz;
 
-use baza;
+use formularz;
 
-create table users (
+CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+    username VARCHAR(30) UNIQUE,
+    password VARCHAR(30)
 );
-INSERT INTO 'users' ('id' 'username', 'password') VALUES
-    ('admin', 'tescior');
+INSERT INTO users (id, username, password) VALUES
+    ('1' 'admin', 'tescior');
